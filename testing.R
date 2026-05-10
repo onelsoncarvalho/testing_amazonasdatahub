3b26059d16ef6e21bf4d52b0e5b6e3d2e5a6b1d5
Nelson-DevStack <n.devpro@protonmail.com>
2026-05-10 18:00:50 -0400

    more datasets added

--- testing.R
+++ testing.R
@@ -1,4 +1,28 @@
 # install.packages(c("nanoparquet"))
 
-require(nanoparquet)
+require(nanoparquet) # referenciar
 
+folder <- "./data"
+
+
+# Base de dados
+load("./data/agriculture_amazonas.rda")
+
+rda_files <- list.files(folder, pattern = ".rda$")
+load(paste0("./data/", rdaFiles[1]))
+
+for (file in rda_files) {
+  obj_names <- load(file.path(folder, file))
+  
+  for (obj_name in obj_names) {
+    df <- get(obj_name)
+    
+    output_name <- paste0(folder, "/", obj_name, ".parquet")
+    write_parquet(df, output_name)
+  }
+  rm(list = obj_names)
+}
+  
+
+
+# write_parquet(agriculture_amazonas, "agriculture_amazonas.parquet")
